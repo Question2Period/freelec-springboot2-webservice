289c1dca98910144f9e5ac0e74824453eaa57cc4
Question2Period <chldnxo3@naver.com>
2022-07-23 22:31:25 +0900

    learn how to estimate runtime of application

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,7 +1,10 @@
 package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+//리스너 기능을 활성화하여 사용시간 체크 가능
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.jojoldu.book.springboot.web;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
 import com.jojoldu.book.springboot.web.dto.PostSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -53,4 +57,45 @@ public class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
+
+    @Test // 업데이트 기능 확인 테스트
+    public void Posts_수정된다() throws Exception {
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build()); //엔티티에 값 채우기
+
+        Long updateId = savedPosts.getId(); //수정할 부분
+        String expectedTitle = "title2"; //업데이트 값
+        String expectedContent = "content2"; // 업데이트 값2
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build(); // request dto를 수정하여 보냄.
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+        //dto를 엔티티형태로 변환..?
+
+        ResponseEntity<Long> responseEntity = restTemplate.
+                exchange(url, HttpMethod.PUT,requestEntity,Long.class);
+        //rest방식으로 responseEntity 반환
+
+        assertThat(responseEntity.getStatusCode()).
+                isEqualTo(HttpStatus.OK); //정상 request를 얻었을 시
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+        //수정사항이 있는지 확인
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).
+                isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).
+                isEqualTo(expectedContent);
+        //실제 예상값과 업데이트 값이 일치하는지 확인
+
+    }
+
 }
