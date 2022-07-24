d4c36b60380d428a401afb200ec7e49a097ea1d3
Question2Period <chldnxo3@naver.com>
2022-07-24 23:14:39 +0900

    learn query annotation

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ tdd 예제 작성 및 local에서 호스팅하여 hello 페이지를 확인했�
 # day 2
 
 롬복 연동 및 dto 클래스를 생성하여 맵핑 및 tdd에도 dto테스트 코드를 작성하였다.
+
+# ...day 3++ 
+![조회기능](https://user-images.githubusercontent.com/93043822/180651020-3b6ea1e8-04c2-4147-a527-115958c57b30.JPG)
+
+게시글 조회 기능까지 구현해보았다.(~151p)
+
--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+    //SpringDataJpa에서 제공하지 않는 메서드이기 때문에 쿼리 어노테이션 사용.
 }
